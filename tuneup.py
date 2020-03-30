e58f32bbb375851a543f86288ae778ed71940927
Matrixmonet <successismyne@gmail.com>
2020-03-30 11:53:29 -0400

    Initial Commit

--- tuneup.py
+++ tuneup.py
@@ -1,9 +1,11 @@
+
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Tuneup assignment"""
 
-__author__ = "Sarah Gale"
+__author__ = "Sarah Gale, Googlefu"
 
+import timeit
 import cProfile
 import pstats
 import functools
@@ -14,7 +16,16 @@ def profile(func):
     # You need to understand how decorators are constructed and used.
     # Be sure to review the lesson material on decorators, they are used
     # extensively in Django and Flask.
-    raise NotImplementedError("Complete this decorator function")
+    def inner(src):
+        profiler = cProfile.Profile()
+        profiler.enable()
+        result = func(src)
+        profiler.disable()
+
+        ps = pstats.Stats(profiler).sort_stats('cumulative')
+        ps.print_stats(10)
+        return result
+    return inner
 
 
 def read_movies(src):
@@ -26,12 +37,18 @@ def read_movies(src):
 
 def is_duplicate(title, movies):
     """returns True if title is within movies list"""
-    for movie in movies:
-        if movie.lower() == title.lower():
-            return True
-    return False
+    # title = title.lower()
+    # for movie in movies:
+    #     if movie.lower() == title.lower():
+    #         return True
+    # return False
+    if title in movies:
+        return True
+    else:
+        return False
 
 
+@profile
 def find_duplicate_movies(src):
     """Returns a list of duplicate movies from a src list"""
     movies = read_movies(src)
@@ -45,7 +62,17 @@ def find_duplicate_movies(src):
 
 def timeit_helper():
     """Part A:  Obtain some profiling measurements using timeit"""
-    # YOUR CODE GOES HERE
+    # bring funct into timer obj
+    t = timeit.Timer(
+        stmt="find_duplicate_movies('movies.txt')",
+        setup="from __main__ import find_duplicate_movies"
+    )
+    t.unit = 'sec'
+    # best time on a per function basis
+    result = min(t.repeat(repeat=7, number=3))/3.0
+    # print(result)
+    print(
+        'Best time across 7 repeats of 3 runs per repeat: {:.3f} secs.'.format(result))
 
 
 def main():
@@ -57,3 +84,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    # timeit_helper()
